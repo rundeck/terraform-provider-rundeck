d55ef4ddbc80a5e87a9001ccb419aca217fe90bd
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-06 12:35:23 -0400

    Initial transfer of provider code

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-rundeck/rundeck"
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: rundeck.Provider})
 }
