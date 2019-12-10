911276c7beaca2fd74b5293dd9fd4fd0ca9d3687
Rob Lazzurs <rob@lazzurs.org>
2019-12-10 00:16:58 +0000

    Migrating to the Terraform Plugin SDK.
    Migrating to the Terraform Plugin SDK using the automated migration
    tool. Build and tests pass. No errors with the migration tool.

--- rundeck/resource_public_key_test.go
+++ rundeck/resource_public_key_test.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/rundeck/go-rundeck/rundeck"
 
-	"github.com/hashicorp/terraform/helper/resource"
-	"github.com/hashicorp/terraform/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccPublicKey_basic(t *testing.T) {
